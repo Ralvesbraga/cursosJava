0514b5032c233e167f25a5db0b4e3b5d17f91d84
Rafael Braga <ralvesbraga40@gmail.com>
2024-05-23 13:54:41 -0500

    Exercise_set resolution 2

--- JavaVS/src/generics_set_map/exercicio_resolvido_set/application/Program.java
+++ JavaVS/src/generics_set_map/exercicio_resolvido_set/application/Program.java
@@ -3,9 +3,13 @@ package generics_set_map.exercicio_resolvido_set.application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.LinkedHashSet;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
+
+import generics_set_map.exercicio_resolvido_set.entities.LogEntry;
 
 public class Program {
     
@@ -14,17 +18,20 @@ public class Program {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter file full path: ");
-        String path = sc.next();
-        Set<String> set = new LinkedHashSet<>();
-        
+        String path = sc.next();      
 
         try(BufferedReader br = new BufferedReader(new FileReader(path))){
 
+            Set<LogEntry> set = new HashSet<>();
             String line = br.readLine(); // lê uma linha e armazena na string line
 
             while(line != null){
                 String[] fields = line.split(" ");
-                set.add(fields[0]);
+                String username = fields[0];
+                Date moment = Date.from(Instant.parse(fields[1]));
+
+                set.add(new LogEntry(username, moment));
+
                 line = br.readLine();
             }
 
@@ -35,6 +42,7 @@ public class Program {
             
         } catch(IOException e){
             System.out.println("ERROR: " + e.getMessage());
+            sc.close();
         }
     }
 }
--- JavaVS/src/generics_set_map/exercicio_resolvido_set/entities/Usuarios.java
+++
@@ -1,9 +0,0 @@
-package generics_set_map.exercicio_resolvido_set.entities;
-
-import java.time.LocalDateTime;
-
-public class Usuarios {
-    private String name;
-    private LocalDateTime acesso;
-
-}
