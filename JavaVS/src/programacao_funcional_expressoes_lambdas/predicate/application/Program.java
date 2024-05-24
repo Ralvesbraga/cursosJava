7a1dec87e5342938f4fbf41087df91d824755691
Rafael Braga <ralvesbraga40@gmail.com>
2024-05-24 16:30:07 -0500

    Non static method - predicate

--- JavaVS/src/programacao_funcional_expressoes_lambdas/predicate/application/Program.java
+++ JavaVS/src/programacao_funcional_expressoes_lambdas/predicate/application/Program.java
@@ -18,7 +18,7 @@ public class Program {
         list.add(new Product("HD case", 80.90));
         list.add(new Product("Mouse", 50.00));
 
-        list.removeIf(Product::staticProductPredicate); //referencia para método, usando isso chama o método da classe product sem precisar de uma instancia 
+        list.removeIf(Product::nonStaticProductPredicate); //referencia para método, usando isso chama o método da classe product sem precisar de uma instancia 
 
         for(Product p : list){
             System.out.println(p);
