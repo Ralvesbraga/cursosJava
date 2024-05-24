1649315d484be65e911f5e4058eb2901ab9c3b95
Rafael Braga <ralvesbraga40@gmail.com>
2024-05-24 16:27:50 -0500

    Method reference static

--- JavaVS/src/programacao_funcional_expressoes_lambdas/predicate/application/Program.java
+++ JavaVS/src/programacao_funcional_expressoes_lambdas/predicate/application/Program.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import programacao_funcional_expressoes_lambdas.predicate.entities.Product;
-import programacao_funcional_expressoes_lambdas.predicate.util.ProductPredicate;
+
 
 public class Program {
     
@@ -18,7 +18,7 @@ public class Program {
         list.add(new Product("HD case", 80.90));
         list.add(new Product("Mouse", 50.00));
 
-        list.removeIf(new ProductPredicate()); //remove qualquer produto da lista que tenha o preço superior a 100 
+        list.removeIf(Product::staticProductPredicate); //referencia para método, usando isso chama o método da classe product sem precisar de uma instancia 
 
         for(Product p : list){
             System.out.println(p);
