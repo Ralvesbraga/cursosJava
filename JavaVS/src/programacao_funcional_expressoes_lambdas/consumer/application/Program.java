90d71448916b91b99bc89c2bf72c0ac1a52f9035
Rafael Braga <ralvesbraga40@gmail.com>
2024-05-27 14:58:16 -0500

    Studying Consumer

---
+++ JavaVS/src/programacao_funcional_expressoes_lambdas/consumer/application/Program.java
@@ -0,0 +1,28 @@
+package programacao_funcional_expressoes_lambdas.consumer.application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import programacao_funcional_expressoes_lambdas.consumer.util.PriceUpdate;
+import programacao_funcional_expressoes_lambdas.consumer.entities.Product;
+
+
+public class Program {
+    
+    public static void main(String[] args) {
+        
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+        list.add(new Product("HD case", 80.90));
+        list.add(new Product("Mouse", 50.00));
+
+        list.forEach(new PriceUpdate());
+
+        list.forEach(System.out::println);
+
+
+    }
+}
---
+++ JavaVS/src/programacao_funcional_expressoes_lambdas/consumer/util/PriceUpdate.java
@@ -0,0 +1,19 @@
+package programacao_funcional_expressoes_lambdas.consumer.util;
+
+
+import java.util.function.Consumer;
+
+import programacao_funcional_expressoes_lambdas.consumer.entities.Product;
+
+public class PriceUpdate implements Consumer<Product>{
+
+    @Override
+    public void accept(Product p) {
+        p.setPrice(p.getPrice() * 1.1);
+    }
+
+
+    
+    
+
+}
